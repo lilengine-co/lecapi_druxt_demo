a8a793d5198a6e099ac29a734bc10cd4782f74c6
feb <dinhhien102@gmail.com>
2020-11-25 14:02:29 +1100

    Updated the card image style

--- components/druxt/entity/ParagraphCards.vue
+++ components/druxt/entity/ParagraphCards.vue
@@ -6,7 +6,7 @@
         <ul class="card__list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <li class="card__item" v-for="card in cards" :key="card.id">
             <div class="card bg-gray-200 rounded-xl p-2 sm:p-5 xl:p-6">
-              <img class="card__image mb-2" v-bind:src="card.image" />
+              <div class="w-full h-48 bg-center bg-cover mb-2" v-bind:style="'background-image:url(' + card.image + ')'"></div>
               <h3 class="card__title text-2xl font-semibold mb-6">{{ card.heading }}</h3>
               <div class="card__content markup__html" v-html="card.content"></div>
               <NuxtLink v-if="card.link" class="button bg-blue-900 hover:bg-red-600 card__link" :to="card.link.uri">
