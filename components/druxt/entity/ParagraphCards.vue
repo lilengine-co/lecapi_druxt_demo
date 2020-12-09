24162dccbc515e8ae94f07175a1f0effce1abcb0
Feb <dinhhien102@gmail.com>
2020-12-09 17:35:36 +1100

    Updated the card variant

--- components/druxt/entity/ParagraphCards.vue
+++ components/druxt/entity/ParagraphCards.vue
@@ -3,10 +3,10 @@
     <div class="card-block block-space">
       <div class="container mx-auto">
         <slot name="subtitle"/>
-        <ul class="card__list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ul class="card__list grid gap-4" :class="gridClass">
           <li class="card__item" v-for="card in cards" :key="card.id">
-            <div class="card bg-gray-200 h-full rounded-xl overflow-hidden">
-              <div class="w-full h-48 bg-center bg-cover" v-bind:style="'background-image:url(' + card.image + ')'"></div>
+            <div class="card bg-gray-100 h-full" :class="getVariant">
+              <div class="w-full bg-center bg-cover" :class="imageClass" v-bind:style="'background-image:url(' + card.image + ')'"></div>
               <div class="card__content p-5 xl:p-6">
                 <h3 class="card__title block__title">{{ card.heading }}</h3>
                 <div class="card__content markup__html" v-html="card.content"></div>
@@ -37,15 +37,15 @@ export default {
       let link = false
       let imageUrl = false
 
-      if(result.relationships._media) {
+      if(result.relationships._media.data) {
         const media = result.relationships._media.data
         const imageObj = await this.getResource(media)
-        const image = await this.getResource(
-          imageObj.relationships.media_image.data
-        )
-
-        imageUrl = process.env.BASE_URL + image.attributes.uri.url
-        // console.log(util.inspect(image, false, null, true))
+        if(imageObj.relationships.media_image) {
+          const image = await this.getResource(
+            imageObj.relationships.media_image.data
+          )
+          imageUrl = process.env.BASE_URL + image.attributes.uri.url
+        }
       }
 
       let heading = result.attributes._heading
@@ -73,15 +73,26 @@ export default {
           linkLabel = linkText ? linkText : entity.attributes.title
           content = content ? content : desk
           linkUri = entity.attributes.path.alias
+
+          if(!heading) {
+            heading = entity.attributes.title
+          }
+
+          if(!imageUrl && entity.relationships.media.data) {
+            let media = entity.relationships.media.data
+            let mediaObj = await this.getResource(media)
+            if(mediaObj.relationships.media_image) {
+              const image = await this.getResource(
+                mediaObj.relationships.media_image.data
+              )
+              imageUrl = process.env.BASE_URL + image.attributes.uri.url
+            }
+          }
         }
 
         link = {
           uri: linkUri,
           text: linkLabel
-        }
-
-        if(!heading) {
-          heading = linkLabel
         }
       }
 
@@ -96,7 +107,9 @@ export default {
     }
   },
   data: () => ({
-    cards: false
+    cards: false,
+    imageClass: 'h-48',
+    gridClass: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3'
   }),
   methods: {
     ...mapActions({
@@ -104,6 +117,19 @@ export default {
       getRoute: 'druxtRouter/getRoute',
       getResource: 'druxtRouter/getEntity',
     })
+  },
+  computed: {
+    getVariant() {
+      let variant = this.entity.attributes.behavior_settings.lecapi_variants.variant
+      if(variant == 'horizontal') {
+        this.imageClass = 'h-full'
+        this.gridClass = 'grid-cols-1 md:grid-cols-2'
+        return "grid grid-cols-1 md:grid-cols-2 gap-2"
+      }
+      else {
+        return ""
+      }
+    }
   }
 }
 </script>
