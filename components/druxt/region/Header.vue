8c155a0e23b77723155993ae4938d105f327937a
Feb <dinhhien102@gmail.com>
2020-12-14 09:53:08 +1100

    Updated logo url

--- components/druxt/region/Header.vue
+++ components/druxt/region/Header.vue
@@ -3,7 +3,7 @@
   <div class="container mx-auto">
     <nav class="flex items-center p-3 flex-wrap">
       <a href="/" class="p-2 mr-4 inline-flex items-center">
-        <img class="h-24 w-auto mr-2" src="http://plumbontennant.com.au/wp-content/uploads/2013/09/plumb1.gif" alt="LilEngine website">
+        <img class="h-24 w-auto mr-2" src="/logo.gif" alt="LilEngine website">
       </a>
       <DruxtMenu name="cafe-menu" class="inline-flex lg:flex-grow top-navbar ml-auto relative lg:w-auto" />
     </nav>
