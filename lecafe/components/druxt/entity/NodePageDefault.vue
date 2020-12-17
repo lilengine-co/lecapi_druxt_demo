f3221adff59b1754470ff0d87a4637e1c4237765
Feb <dinhhien102@gmail.com>
2020-12-17 16:16:56 +1100

    Updated the condition

--- lecafe/components/druxt/entity/NodePageDefault.vue
+++ lecafe/components/druxt/entity/NodePageDefault.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div v-if="fields.deck.data.value || entity.attributes.title" class="container mx-auto text-center">
+    <div v-if="fields.deck || entity.attributes.title" class="container mx-auto text-center">
       <h1 v-if="entity.attributes.title" class="page-title text-4xl mb-2">{{ entity.attributes.title }}</h1>
-      <div v-if="fields.deck.data.value" class="page__deck text-xl" v-html="fields.deck.data.value"></div>
+      <div v-if="fields.deck" class="page__deck text-xl" v-html="fields.deck.data.value"></div>
       <!-- <slot name="hero" /> -->
     </div>
     <div class="mt-10">
