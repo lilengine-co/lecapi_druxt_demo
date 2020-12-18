738fba6393b01777fca0f40e3ba9d1f5b889c4c6
Feb <dinhhien102@gmail.com>
2020-12-18 11:45:57 +1100

    Updated the image url for media field

---
+++ lecafe/components/druxt/field/ImageMediaImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="!$fetchState.pending">
+    <img
+      v-for="entity of entities"
+      :key="entity.id"
+      :src="entity.attributes.image_style_uri.large"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+import { DruxtFieldResponsiveImage } from 'druxt-entity'
+
+export default {
+  extends: DruxtFieldResponsiveImage,
+}
+</script>
